edb375e5cb4c67880d799a941e16e8fa55b9da88
Arthur BLANC <arthur.blanc@outlook.com>
2022-06-14 16:25:45 +0200

    add createReducer for api.jsx

--- Front-end/src/store/api.jsx
+++ Front-end/src/store/api.jsx
@@ -1,9 +1,7 @@
+import { createReducer } from "@reduxjs/toolkit";
+
 const initialState = {
 	baseURL: "http://localhost:3001/api/v1",
 };
 
-const apiReducer = (state = initialState) => {
-	return state;
-};
-
-export default apiReducer;
+export default createReducer(initialState, (builder) => builder);
